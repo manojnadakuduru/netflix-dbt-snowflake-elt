9a47e295d82ea7fa7a10e13661c6bda51f2ae221
Manoj Nadakuduru <manojnadakuduru@gmail.com>
2026-02-13 00:28:03 -0500

    Add files via upload

--- Netflix/tests/relevance_score_test.sql
+++ Netflix/tests/relevance_score_test.sql
@@ -1,1 +1,10 @@
+-- This test ensures that all order amounts are positive
+{# SELECT
+    movie_id,
+    tag_id,
+    relevance_score
+FROM {{ ref('fct_genome_scores') }}
+WHERE relevance_score <= 0 #}
 
+--Let's test macros by commenting the above code.
+{{no_nulls_in_columns(ref('fct_genome_scores'))}}
