3d946bf699833d2bfd46c057875c76be3aa78dbd
Manoj Nadakuduru <manojnadakuduru@gmail.com>
2026-02-13 00:30:21 -0500

    Add files via upload

--- Netflix/macros/no_nulls_in_columns.sql
+++ Netflix/macros/no_nulls_in_columns.sql
@@ -1,1 +1,11 @@
+{% macro no_nulls_in_columns(model) %}
+    SELECT * FROM {{ model }} WHERE
+    {% for col in adapter.get_columns_in_relation(model) %}
+        {{ col.column }} IS NULL OR
+    {% endfor %}
+    FALSE
+{% endmacro %}
 
+--Earlier for testing we have used singular tests 
+-- We can do the same by using macros as well
+--Let's use this macros in relevance_score_test under tests
